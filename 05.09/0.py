53bcce2ccb950705887bb4f1e40a0987763750eb
Laura Nunes <lauracostanunes2@gmail.com>
2025-09-12 19:38:19 -0300

    0.py

---
+++ 05.09/0.py
@@ -0,0 +1,11 @@
+x = int(input("Insira um numero :"))
+
+if x == 0:
+    print(f"O valor {x} é igual a zero")
+else:
+    if x > 0:
+        print(f"O valor {x} é positivo")# entra no else
+    else:
+        print(f"O valor {x} é negativo") # else do if acima apenas
+
+print("Fim do pograma")
