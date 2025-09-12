b97c6bc19b499391e0b7b23dcaeeec8aba887b0f
Laura Nunes <lauracostanunes2@gmail.com>
2025-09-12 19:40:57 -0300

    Update If.py

---
+++ 05.09/If.py
@@ -0,0 +1,10 @@
+#if e else em python, só é eecutada se if for verdadeiro, s efor false, ele nao executa
+x = int(input("Digite um numero"))
+
+if x > 0:
+   print("positivo") # nao pode colocar espaço, por nao ter chave, tem q deixar alinhadinho
+   
+else:
+   print("errou bobão")
+
+print("Fim do pograma")# desse modo ele aparece no if também, nos dois, ao final
