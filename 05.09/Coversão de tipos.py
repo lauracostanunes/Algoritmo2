24fc3208a7343fcb67361e6cfee9ba1d6547a152
Laura Nunes <lauracostanunes2@gmail.com>
2025-09-12 19:39:09 -0300

    Coversão de tipos.py

---
+++ 05.09/Coversão de tipos.py
@@ -0,0 +1,14 @@
+a = 2.33
+#conversao de numero para string
+print(str(a))
+
+b = "333"
+print(b)
+
+#conversao de string para inteiro
+c = int(b) + 2
+print(c)
+
+n = 7 / 3
+print(" n arredondado", round(n, 2))
+print(" n sem arredondar", n)
