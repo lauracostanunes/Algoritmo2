590a3ecbbf7c3ac01e5fff12db02be2ef13a155d
Laura Nunes <lauracostanunes2@gmail.com>
2025-09-12 19:42:04 -0300

    Aula 1.py

---
+++ 05.09/Aula 1.py
@@ -0,0 +1,13 @@
+print ("Hello")
+
+x = 10 # no python, a variael é declarada dando um valor para ela, indicando seu tipo
+
+print(type(x)) #type mostra o tipo da variavel
+
+print(" x vale: ", x)
+
+y = 33.3
+print(type(y))
+
+a = 'facef'
+print(type(a))
