eb345752019259090b9b6dfab73f6583a342e5cb
Laura Nunes <lauracostanunes2@gmail.com>
2025-09-12 19:42:28 -0300

    Nome.py

---
+++ 05.09/Nome.py
@@ -0,0 +1,12 @@
+idade = 19
+altura = 1.73
+print(" A idade é: ", idade, "a altura é: ", altura)
+print(f' A idade é {idade}, e a altura é {altura}') #mostrar variavel ja na string, q nem no js
+
+
+nome = input("Digite seu nome:")
+print("Seu nome é: ", nome)
+
+numero = input(" Digite o número:")
+print("Número é: ", numero)
+print(type(numero)) 
