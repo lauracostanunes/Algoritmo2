6fbf6c018b9a98135e4c266ba8e2b4fe92a2bbb2
Laura Nunes <lauracostanunes2@gmail.com>
2025-10-03 22:11:31 -0300

    Aula 03/10 - Python

---
+++ 03.10/Range.py
@@ -0,0 +1,13 @@
+for x in range (5):
+    print("Valor = ", x)
+
+for n in range (5, 10):
+    print (n)
+
+n = range(10)
+for x in n:
+    print(x)
+
+n = range(3, 11, 3)
+for x in n:
+    print(x)
---
+++ 03.10/Tabuada.py
@@ -0,0 +1,3 @@
+numero = int(input("Digite um número: "))
+for n in range (0, 11):
+        print(f"{numero} x {n} = {numero * n}")
