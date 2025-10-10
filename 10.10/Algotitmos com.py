468e944cd96667faa8ca63f61179ea2c29eb0968
Laura Nunes <lauracostanunes2@gmail.com>
2025-10-10 19:57:07 -0300

    Aula 10/10 - Python

---
+++ 10.10/Algotitmos com.py
@@ -0,0 +1,20 @@
+# Atribuições
+# Condicionas
+# Repetição:
+    # for
+    # while
+# Listas
+
+# Listas:
+    # Indíce => n-1
+    # listas[indice]
+    # atribuir
+    # acessar
+
+# Tornando lista
+    # len(lista)
+
+# For
+    # Percorre/ itera uma lista
+        # for <<var>> in <<lista>>:
+    # Não precisa especificar início, nem fim
---
+++ 10.10/Lista de compras.py
@@ -0,0 +1,11 @@
+compras = ["Arroz", "Feijão", "Carne", 12.2]
+print(compras)
+print(compras[3])
+compras[3] = 99.9
+print(len(compras))
+compras[4] = "Chocolate"
+print(compras)
+compras = []
+print(compras)
+x = list(range[5])
+print(x)
