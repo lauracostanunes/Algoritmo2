09717619f80f0e74966a32fafc171c19f06bc9dc
Laura Nunes <lauracostanunes2@gmail.com>
2025-10-24 21:35:51 -0300

    Aula 24/10 - Python

--- 24.10/Tuplas.py
+++ 24.10/Tuplas.py
@@ -2,10 +2,13 @@
 # Listas são mutáveis
 # Tuplas são imutáveis
 
-aluno = ["João da Silva", 111, 23.40]
+aluno = ("João da Silva", 111, 23.40)
 print(aluno)
 print("Acesso posicional da tupla:")
 print("Nome:", aluno[0])
 print("Código:", aluno[1])
 print("Nota:", aluno[2])
 
+aluno2 = ("Maria", 222, 33.22)
+alunos = aluno + aluno2
+print(alunos)
