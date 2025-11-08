bdf6b1404525ee9685ee40f4c4bfdb838e8c36cd
Laura Nunes <lauracostanunes2@gmail.com>
2025-11-07 21:56:02 -0300

    Aula 07/11 - Python

--- 07.11/Lista de números.py
+++ 07.11/Lista de números.py
@@ -1,6 +1,7 @@
 def calcularMedia(numeros):
     return sum(numeros)/len(numeros)
 def mediaPonderada(numeros, pesos):
+    soma = 0
     for n in range(len(numeros)):
         soma += numeros[n] * pesos[n]
         return soma/sum(pesos)
@@ -9,3 +10,6 @@ pesos = [1, 2, 2, 3, 1]
 print("Tamanho:", len(lista))
 media = calcularMedia(lista)
 print("Média:", media)
+mediaP = mediaPonderada(lista, pesos)
+print("Média ponderada:", mediaP)
+print("Razão:", media/mediaP)
